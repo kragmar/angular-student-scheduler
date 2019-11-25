8eaf830774f2d256dca7e91ed09459deebe99b2b
kragmar <kragmar97@gmail.com>
2019-11-25 18:18:33 +0100

    3 files modified

--- src/app/mock-lessons.ts
+++ src/app/mock-lessons.ts
@@ -4,7 +4,10 @@ export const LESSONS: Lesson[] = [
   { id: 1, name: 'Első' },
   { id: 2, name: 'Második' },
   { id: 3, name: 'Harmadik' },
-  { id: 4, name: 'Első' },
-  { id: 2, name: 'Második' },
-  { id: 5, name: 'Harmadik' }
+  { id: 3, name: 'Negyedik' },
+  { id: 2, name: 'Ötödik' },
+  { id: 1, name: 'Hatodik' },
+  { id: 1, name: 'Hetedik' },
+  { id: 3, name: 'Nyolcadik' },
+  { id: 1, name: 'Kilencedik' },
 ];
