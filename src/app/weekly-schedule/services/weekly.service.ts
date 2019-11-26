882df57d18613bb3a769fe8a7ce845778422d479
kragmar <kragmar97@gmail.com>
2019-11-26 15:15:24 +0100

    3 files modified

--- src/app/weekly-schedule/services/weekly.service.ts
+++ src/app/weekly-schedule/services/weekly.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs/Observable';
 
-@Injectable()
+import { Lesson } from '../lesson';
+import { LESSONS } from './mock-data';
+
+@Injectable({
+  providedIn: 'root'
+})
 export class WeeklyService {
 
   constructor() { }
+
+  getLessons(): Observable<Lesson[]> {
+    return of(LESSONS);
+  }
 
 }
--- src/app/weekly-schedule/table/table.component.ts
+++ src/app/weekly-schedule/table/table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Lesson } from '../lesson';
+import { WeeklyService } from '../services/weekly.service';
 
 @Component({
   selector: 'app-table',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
-  constructor() { }
+  lessons: Lesson[];
+
+  constructor(private weeklyService: WeeklyService) { }
 
   ngOnInit() {
   }
