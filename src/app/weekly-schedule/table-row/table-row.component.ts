1091514147aca21fa63791923386f74aa1f64d4d
kragmar <kragmar97@gmail.com>
2019-11-27 13:06:36 +0100

    5 files modified

--- src/app/weekly-schedule/table-row/table-row.component.ts
+++ src/app/weekly-schedule/table-row/table-row.component.ts
@@ -16,46 +16,4 @@ export class TableRowComponent implements OnInit {
   ngOnInit() {
   }
 
-  setCol(lesson: Lesson): string {
-    switch(lesson.id) {
-      case 1:
-        return "table__body__row__cell--col1";
-      case 2:
-        return "table__body__row__cell--col2";
-      case 3:
-        return "table__body__row__cell--col3";
-      case 4:
-        return "table__body__row__cell--col4";
-      case 5:
-        return "table__body__row__cell--col5";
-      case 6:
-        return "table__body__row__cell--col6";
-      case 7:
-        return "table__body__row__cell--col7";
-      case 8:
-        return "table__body__row__cell--col8";
-      case 9:
-        return "table__body__row__cell--col9";
-      default:
-        return "";
-    }
-  }
-
-  setRow(lesson: Lesson): string {
-    switch(lesson.lessonStart) {
-      case '1':
-        return "table__body__row__cell--row1";
-      case '2':
-        return "table__body__row__cell--row2";
-      case '3':
-        return "table__body__row__cell--row3";
-      case '4':
-        return "table__body__row__cell--row4";
-      case '5':
-        return "table__body__row__cell--row5";
-      default:
-        return "";
-    }
-  }
-
 }
