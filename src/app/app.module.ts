d9992d3a10a37d4dd31e4c4080c22f41a2ea1575
kragmar <kragmar97@gmail.com>
2019-11-28 18:01:25 +0100

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material';
+import { MatNativeDateModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,8 +25,10 @@ import { WeeklyService } from './weekly-schedule/services/weekly.service';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
-  providers: [WeeklyService],
+  providers: [WeeklyService, MatDatepickerModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
