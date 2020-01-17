31807191369e2889eec121b203f4f70627ae4db0
kragmar <kragmar97@gmail.com>
2020-01-17 16:58:19 +0100

    1 file modified

--- src/app/services/students.service.ts
+++ src/app/services/students.service.ts
@@ -1,51 +1,68 @@
 import { Injectable } from '@angular/core';
 import { Student } from './student';
-import { Http, Response } from '@angular/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class StudentsService {
 
   private studentsUrl = '/api/students';
 
-  constructor(private http: Http) { }
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient) { }
 
   // get("/api/students")
-  getStudents(): Promise<void | Student[]> {
-    return this.http.get(this.studentsUrl)
-                    .toPromise()
-                    .then(response => response.json() as Student[])
-                    .catch(this.handleError);
+  getStudents(): Observable<Student[]> {
+    return this.http.get<Student[]>(this.studentsUrl)
+                    .pipe(
+                      catchError(this.handleError<Student[]>('getStudents', []))
+                    );
   }
 
   // post("/api/students")
-  createStudent(newStudent: Student): Promise<void | Student> {
-    return this.http.post(this.studentsUrl)
-                    .toPromise()
-                    .then(response => response.json() as Student)
-                    .catch(this.handleError);
+  createStudent(newStudent: Student): Observable<Student> {
+    return this.http.post<Student>(this.studentsUrl, newStudent, this.httpOptions)
+                    .pipe(
+                      catchError(this.handleError<Student>('createStudent'))
+                    );
   }
 
   // delete("/api/students/:id")
-  deleteStudent(delStudentId: String): Promise<void | String> {
-    return this.http.delete(this.studentsUrl + '/' + delStudentId)
-                    .toPromise()
-                    .then(response => response.json() as String)
-                    .catch(this.handleError);
+  deleteStudent(delStudentId: String): Observable<Student> {
+    const url = this.studentsUrl + '/' + delStudentId;
+    return this.http.delete<Student>(url, this.httpOptions)
+                    .pipe(
+                      catchError(this.handleError<Student>('deleteStudent'))
+                    );
   }
 
   // put("/api/students/:id")
-  updateStudent(putStudent: Student): Promise<void | Student> {
-    var putUrl = this.studentsUrl + '/' + putStudent._id;
-    return this.http.put(putUrl, putStudent)
-                    .toPromise()
-                    .then(response => response.json() as Student)
-                    .catch(this.handleError);
+  updateStudent(putStudent: Student): Observable<Student> {
+    return this.http.put(this.studentsUrl, putStudent, this.httpOptions)
+                    .pipe(
+                      catchError(this.handleError<any>('updateStudent'))
+                    );
   }
 
-  private handleError (error: any) {
-    let errMsg = (error.message) ? error.message :
-    error.status ? `${error.status} - ${error.statusText}` : 'Server error';
-    console.error(errMsg); // log to console instead
-  }
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+private handleError<T> (operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
+}
 
 }
