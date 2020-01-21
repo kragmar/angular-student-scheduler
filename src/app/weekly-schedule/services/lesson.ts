2cef0eb6d92ea027b1a1a05f10eb7649143959b1
kragmar <kragmar97@gmail.com>
2020-01-21 18:24:06 +0100

    1 file modified, 3 deleted

--- src/app/weekly-schedule/services/lesson.ts
+++
@@ -1,8 +0,0 @@
-export class Lesson {
-  id: number;
-  lessonDate: string;
-  lessonStart: string;
-  lessonType: string;
-  student: string;
-  teacher: string;
-}
--- src/app/weekly-schedule/table/table.component.ts
+++ src/app/weekly-schedule/table/table.component.ts
@@ -131,7 +131,7 @@ export class TableComponent implements OnInit {
 
   setRow(lesson: Lesson, date: Date): Lesson[] {
     let lessonDate = lesson.lessonDate;
-    if(lessonDate.getTime() == date.getTime()) {
+    if(lessonDate.getDate() == date.getDate()) {
       return new Array<Lesson>(lesson);
     }
     return ;
