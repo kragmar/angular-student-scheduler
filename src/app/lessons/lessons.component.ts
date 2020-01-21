76299d5c69fc8cf17c86d7d8e6e18242c1ffeb85
kragmar <kragmar97@gmail.com>
2020-01-21 19:05:20 +0100

    1 file modified

--- src/app/lessons/lessons.component.ts
+++ src/app/lessons/lessons.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Lesson } from '../services/lesson';
 import { LessonsService } from '../services/lessons.service';
+import { Student } from '../services/student';
+import { StudentsService } from '../services/students.service';
 import { SaveDialogComponent } from '../save-dialog/save-dialog.component';
 
 import { MatDialog } from '@angular/material';
@@ -17,18 +19,26 @@ export class LessonsComponent implements OnInit {
   lessonTimes: string[] = ["12:50", "13:40", "14:30", "15:20", "16:10", "17:00", "17:50", "18:40", "19:30"];
   lessonTypes: string[] = ["Tanóra", "Gyakorló"];
 
+  students: Student[];
+
   showVarNew: boolean;
   showVarDelete: boolean;
 
-  constructor(private lessonsService: LessonsService, private dialog: MatDialog) { }
+  constructor(private lessonsService: LessonsService, private studentsService: StudentsService, private dialog: MatDialog) { }
 
   ngOnInit() {
     this.getLessons();
+    this.getStudents();
   }
 
   getLessons(): void {
     this.lessonsService.getLessons()
                        .subscribe(lessons => this.lessons = lessons);
+  }
+
+  getStudents(): void {
+    this.studentsService.getStudents()
+                        .subscribe(students => this.students = students);
   }
 
   add(): void {
