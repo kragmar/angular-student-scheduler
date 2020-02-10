ae0b53183504cdaed7670410e7cfda9b9d09734d
kragmar <kragmar97@gmail.com>
2020-02-10 18:56:29 +0100

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { UpdateDialogComponent } from './update-dialog/update-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Student Scheduler';
+
+  constructor(private dialog: MatDialog) { 
+    this.openDialog();
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(UpdateDialogComponent, {panelClass: 'update-dialog-box'});
+  }
+
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material';
-import { UpdateDialogComponent } from '../update-dialog/update-dialog.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,15 +7,9 @@ import { UpdateDialogComponent } from '../update-dialog/update-dialog.component'
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private dialog: MatDialog) { 
-    this.openDialog();
-   }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  openDialog(): void {
-    const dialogRef = this.dialog.open(UpdateDialogComponent, {panelClass: 'update-dialog-box'});
   }
 
 }
