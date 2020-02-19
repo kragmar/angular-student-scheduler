d05ec25e75cf8fb552b0d5d34da087c5b49f2ca8
kragmar <kragmar97@gmail.com>
2020-02-19 18:14:05 +0100

    1 file modified

--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -21,7 +21,7 @@ export class StudentsComponent implements OnInit {
 
   lessons: Lesson[];
   newLesson: any = {};
-  studentLessons: LessonWithDay;
+  studentLessons: any = {};
   lessonTimes: string[] = ["12:50", "13:40", "14:30", "15:20", "16:10", "17:00", "17:50", "18:40", "19:30"];
   lessonTypes: string[] = ["Tanóra", "Gyakorló"];
   lessonDays: any = {"1": "Hétfő", "2": "Kedd", "3": "Szerda", "4": "Csütörtök", "5": "Péntek"};
